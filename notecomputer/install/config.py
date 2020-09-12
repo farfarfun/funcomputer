bd67ecabef54dc340a585472987bf8e818e4419c
bingtao <niuliangtao@qq.com>
2020-09-12 13:11:58 +0000

    add

--- notecomputer/install/config.py
+++ notecomputer/install/config.py
@@ -3,10 +3,11 @@ from .base import run_cmd
 
 def config_all():
     config_ssh()
-    run_cmd("cp -rf '/content/gdrive/My Drive/core/configs/' '~/'")
+    run_cmd("cp -rf '/content/gdrive/My Drive/core/configs/' '/root/'")
 
 
 def config_ssh():
     # run_cmd("cp -r '/root/.ssh' '/content/gdrive/My Drive/core/configs/ssh'")
-    run_cmd("cp -rf '/content/gdrive/My Drive/core/configs/ssh/id_rsa' '~/.ssh/'")
-    run_cmd("cp -rf '/content/gdrive/My Drive/core/configs/ssh/id_rsa.pub' '~/.ssh/'")
+    run_cmd("cp -rf '/content/gdrive/My Drive/core/configs/ssh/id_rsa' '/root/.ssh/'")
+    run_cmd("cp -rf '/content/gdrive/My Drive/core/configs/ssh/id_rsa.pub' '/root/.ssh/'")
+    run_cmd("cp -rf '/content/gdrive/My Drive/core/configs/root/.pypirc' '/root/.pypirc'")
--- notecomputer/install/core_server.py
+++ notecomputer/install/core_server.py
@@ -1,6 +1,6 @@
 from .base import run_cmd
 
-config_dir = '~/configs/'
+config_dir = '/root/configs/'
 
 
 def install_drive():
@@ -10,10 +10,10 @@ def install_drive():
 
 def install_code_server():
     run_cmd("curl -fsSL https://code-server.dev/install.sh | sh")
-    run_cmd("mkdir -vp ~/logs/code-server/")
+    run_cmd("mkdir -vp /root/logs/code-server/")
 
     run_cmd(
-        "nohup code-server --auth none --config {}code/code-server.yaml >>~/logs/code-server/code-server.log 2>&1 &".format(
+        "nohup code-server --auth none --config {}code/code-server.yaml >>/root/logs/code-server/code-server.log 2>&1 &".format(
             config_dir))
 
 
@@ -21,5 +21,5 @@ def install_natapp():
     run_cmd("wget http://download.natapp.cn/assets/downloads/clients/2_3_9/natapp_linux_amd64/natapp -O natapp")
     run_cmd("chmod a+x natapp")
 
-    run_cmd("mkdir -vp ~/logs/natapp/")
-    run_cmd("nohup ./natapp -authtoken=65a40f94924dc275  >>~/logs/natapp/natapp.log 2>&1 &")
+    run_cmd("mkdir -vp /root/logs/natapp/")
+    run_cmd("nohup ./natapp -authtoken=65a40f94924dc275  >>/root/logs/natapp/natapp.log 2>&1 &")
